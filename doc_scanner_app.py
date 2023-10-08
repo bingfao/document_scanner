6c2a309a3939d36c9875f051912e916a52dd3124
bingfao <bingfao@hotmail.com>
2023-10-08 09:32:23 +0800

    add canvas scrollable

--- doc_scanner_app.py
+++ doc_scanner_app.py
@@ -11,8 +11,8 @@ import argparse
 from PIL import Image
 from PIL import ImageTk
 
-import Tkinter
-import tkFileDialog
+import tkinter
+import tkinter.filedialog 
 
 import numpy as np
 import cv2
@@ -26,8 +26,8 @@ from doc_scanner import (
 class DocScannerWindow(object):
     def __init__(self, master, image_filename=None):
         self.master = master
-        self.origin_image = Image.new('RGB', (640,480), (255, 255, 255))
-        self.image = Image.new('RGB', (640,480), (255, 255, 255))
+        self.origin_image = Image.new('RGB', (1280,960), (255, 255, 255))
+        self.image = Image.new('RGB', (1280,960), (255, 255, 255))
         self.cv_image = None
         self.warped_image = None
         self.file_dir = ""
@@ -43,14 +43,26 @@ class DocScannerWindow(object):
         self.image_tmp = ImageTk.PhotoImage(self.image)
 
         # init four corners of the document
-        self.canvas = Tkinter.Canvas(self.master,
+        frame = tkinter.Frame(self.master)
+        sv = tkinter.Scrollbar(frame)  # 定义垂直滚动条
+        sv.pack(side="right", fill='y')  # 放置垂直滚动条在最右侧,占满Y轴
+        sv2 = tkinter.Scrollbar(frame,orient='horizontal')  # 定义水平滚动条
+        sv2.pack(side="bottom", fill='x')  # 放置水平滚动条在最右侧,占满Y轴
+        self.canvas = tkinter.Canvas(frame,
                 width=self.master.winfo_screenwidth(),
                 height=self.master.winfo_screenheight(),
-                bd=0, highlightthickness=0)
-        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=Tkinter.NW,
+                bd=0, highlightthickness=0,xscrollincrement=100,yscrollincrement=100)
+        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=tkinter.NW,
                 image=self.image_tmp)
         self.canvas.bind('<Motion>', self.on_mouse_move)
+        self.canvas.config(yscrollcommand=sv.set,xscrollcommand=sv2.set)  # 设置画布的Y轴滚动条函数与垂直滚动条绑定 
+        sv.config(command=self.canvas.yview)  # 设置垂直滚动条的函数与画布的Y轴滚动条事件绑定
+        sv2.config(command=self.canvas.xview)  # 设置垂直滚动条的函数与画布的x轴滚动条事件绑定
         self.canvas.pack()
+        frame.pack()
+        self.canvas.config(yscrollincrement=1,xscrollincrement=1)  # 设置滚动条的步长
+        self.canvas.bind("<MouseWheel>", self.event1)  # 添加滚轮事件
+
         self.paint_ovals = [
                 self.canvas.create_oval(0, 0, 0, 0, outline="#f11", fill="#1f1"),
                 self.canvas.create_oval(0, 0, 0, 0, outline="#f11", fill="#1f1"),
@@ -69,25 +81,25 @@ class DocScannerWindow(object):
         self.master.geometry("{0}x{0}+0+0".format(
                 self.master.winfo_screenwidth() - pad,
                 self.master.winfo_screenheight() - pad))
-        self.master.resizable(False, False)  # disable resizing
+        self.master.resizable(True, True)  # disable resizing
 
 
     def init_menubar(self):
-        menubar = Tkinter.Menu(self.master)
-        file_menu = Tkinter.Menu(menubar, tearoff=0)
+        menubar = tkinter.Menu(self.master)
+        file_menu = tkinter.Menu(menubar, tearoff=0)
         file_menu.add_command(label="Open", command=self.open_file)
         file_menu.add_command(label="Save as", command=self.save_file)
         file_menu.add_command(label="Export to PDF", command=self.export2pdf)
         file_menu.add_command(label="Quit", command=self.master.destroy)
         menubar.add_cascade(label="File", menu=file_menu)
 
-        filter_menu = Tkinter.Menu(menubar, tearoff=0)
+        filter_menu = tkinter.Menu(menubar, tearoff=0)
         filter_menu.add_command(label="Find Edges", command=self.edge_detect)
         filter_menu.add_command(label="Image Binarization", command=self.image_binarization)
         filter_menu.add_command(label="Restore Image", command=self.restore_image)
         menubar.add_cascade(label="Filter", menu=filter_menu)
 
-        transform_menu = Tkinter.Menu(menubar, tearoff=0)
+        transform_menu = tkinter.Menu(menubar, tearoff=0)
         transform_menu.add_command(label="Detect Contour", command=self.detect_contour)
         transform_menu.add_command(label="Apply Perspective Transform",
                 command=self.apply_perspective_transform)
@@ -98,7 +110,7 @@ class DocScannerWindow(object):
 
     def open_file(self, filename=None):
         if filename is None:
-            filename = tkFileDialog.askopenfilename(parent=self.master,title='Choose files')
+            filename = tkinter.filedialog.askopenfilename(parent=self.master,title='Choose files')
         if type(filename) is tuple or filename == "":
             return
         self.image = Image.open(filename).convert("RGB")
@@ -118,26 +130,40 @@ class DocScannerWindow(object):
 
 
     def save_file(self):
-        filename = tkFileDialog.asksaveasfilename()
+        filename = tkinter.filedialog.asksaveasfilename()
         if type(filename) is tuple or filename == "":
             return
         self.image.save(filename)
 
 
     def export2pdf(self):
-        filename = tkFileDialog.asksaveasfilename()
+        filename = tkinter.filedialog.asksaveasfilename()
         if type(filename) is tuple or filename == "":
             return
         if not filename.endswith(".pdf"):
             filename += ".pdf"
         self.image.save(filename)
 
+    def event1(self,event):
+        """
+        事件的属性 delta 解析
+        在MouseWheel 事件中,正值代表上卷,负值代表下卷;
+        在 Window 下,通常是 120 的倍数;在 MacOS 下,为 1 的倍数
+        """
+        number = int(-event.delta / 120)
+        self.canvas.yview_scroll(number, 'units')
 
-    def update(self):
-        self.master.geometry('{}x{}'.format(self.image.size[0], self.image.size[1]))
+
+    def update(self):  
+        image_w = self.image.size[0]
+        image_h = self.image.size[1]
+        self.canvas.config(scrollregion=(0, 0, image_w, image_h))  # 设置画布可以滚动的范围
+        
+        self.master.geometry('{}x{}'.format(image_w, image_h))
 
         self.image_tmp = ImageTk.PhotoImage(self.image)
         self.canvas.itemconfig(self.image_on_canvas, image=self.image_tmp)
+
 
         for oval, corner in zip(self.paint_ovals, self.doc_corners):
             x, y = corner[0], corner[1]
@@ -203,7 +229,9 @@ class DocScannerWindow(object):
     def on_mouse_move(self, event):
         x, y = event.x, event.y
         if self.enable_select_corner:
-            self.doc_corners[self.selected_corner_idx] = [x, y]
+            vx= self.canvas.canvasx(x)
+            vy=self.canvas.canvasy(y)
+            self.doc_corners[self.selected_corner_idx] = [vx, vy]
             self.update()
 
 
@@ -214,8 +242,10 @@ class DocScannerWindow(object):
             self.selected_corner_idx = -1
             return
         x, y = event.x, event.y
+        vx= self.canvas.canvasx(x)
+        vy=self.canvas.canvasy(y)
         for idx, corner in enumerate(self.doc_corners):
-            if math.hypot(x-corner[0], y-corner[1]) < 10:
+            if math.hypot(vx-corner[0], vy-corner[1]) < 10:
                 self.selected_corner_idx = idx
                 self.enable_select_corner = True
                 break
@@ -232,6 +262,6 @@ if __name__ == '__main__':
     args = get_args()
     image_filename = args.image
 
-    root = Tkinter.Tk()
+    root = tkinter.Tk()
     doc_scan_window = DocScannerWindow(root, image_filename)
     root.mainloop()
